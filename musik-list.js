50e16a2c20b6dd7fc32a0b8000f22b77cef221e1
Reyhansyah11 <149665520+Reyhansyah11@users.noreply.github.com>
2024-03-07 14:41:49 +0700

    Add files via upload

---
+++ musik-list.js
@@ -0,0 +1,20 @@
+let allMusic = [
+    {
+        name: "Overdose", //nama musik
+        artist: "Kobo Kanaeru", //nama artis
+        img: "music-1", //nama gambar
+        src: "music-1", //nama disini
+    },
+    {
+        name: "Rip my Pants", //nama musik
+        artist: "eclat story", //nama artis
+        img: "music-2", //nama gambar
+        src: "music-2", //nama disini
+    },
+    {
+        name: "Original Song by Chloe", //nama musik
+        artist: "Chloe pawapua", //nama artis
+        img: "music-3", //nama gambar
+        src: "music-3", //nama disini
+    },
+]
